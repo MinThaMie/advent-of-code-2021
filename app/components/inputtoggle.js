9dfe452308e867eb550259d22cbd70efa1186677
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2021-12-08 21:56:35 +0100

    Adds focus to toggle + mobile friendly

--- app/components/inputtoggle.js
+++ app/components/inputtoggle.js
@@ -4,10 +4,20 @@ import { action } from '@ember/object';
 
 export default class InputtoggleComponent extends Component {
   @tracked input = true;
-
+  @tracked focused = false;
   @action
   toggle() {
     this.input = !this.input;
     this.args.toggle?.(this.input);
+  }
+
+  @action
+  handleFocusIn() {
+    this.focused = true;
+  }
+
+  @action
+  handleFocusOut() {
+    this.focused = false;
   }
 }
