73e2b7c482fcd1447ae27a53f02a1ca1071ec762
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2021-12-08 20:55:16 +0100

    Move day 6 to controller

--- app/routes/puzzles/6.js
+++ app/routes/puzzles/6.js
@@ -1,3 +1,18 @@
 import Route from '@ember/routing/route';
 
-export default class Puzzels6Route extends Route {}
+export default class Puzzels6Route extends Route {
+  parseInput(file) {
+    return file.split(',').map(n => parseInt(n));
+  }
+
+  async model() {
+    let resIntro = await fetch('/inputs/day6/intro.txt');
+    let introFile = await resIntro.text();
+    let res = await fetch('/inputs/day6/full.txt');
+    let fullFile = await res.text();
+    return {
+      example: this.parseInput(introFile),
+      full: this.parseInput(fullFile),
+    };
+  }
+}
